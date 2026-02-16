8ae4f39cb58a282dec4c1c0d57d7bf8c94fe73aa
waleedabd037 <waleed.abdullah037@gmail.com>
2026-02-16 21:11:26 +0500

    dynamic urll

--- app/components/PayRow.tsx
+++ app/components/PayRow.tsx
@@ -2,27 +2,37 @@
 
 export default function PayRow({ title, logo, appUrl, webUrl }: any) {
 
-  const handleClick = () => {
-    const userAgent = navigator.userAgent || navigator.vendor;
+const handleClick = () => {
+  const isMobile = /android|iphone|ipad|ipod/i.test(navigator.userAgent);
 
-    const isMobile = /android|iphone|ipad|ipod/i.test(userAgent);
-
-    if (isMobile) {
-      // Try opening app
-      const timeout = setTimeout(() => {
-        // If app doesn't open, fallback to website
-        window.location.href = webUrl;
-      }, 1500);
-
-      window.location.href = appUrl;
-
-      // If user switches app, timeout will naturally stop
-      return;
-    }
-
-    // Desktop → always open website
+  if (!isMobile) {
     window.open(webUrl, "_blank");
+    return;
+  }
+
+  let pageHidden = false;
+
+  const visibilityHandler = () => {
+    if (document.hidden) {
+      pageHidden = true; // app opened
+    }
   };
+
+  document.addEventListener("visibilitychange", visibilityHandler);
+
+  // try opening app
+  window.location.href = appUrl;
+
+  // fallback check
+  setTimeout(() => {
+    document.removeEventListener("visibilitychange", visibilityHandler);
+
+    if (!pageHidden) {
+      window.open(webUrl, "_blank");
+    }
+  }, 1800);
+};
+
 
   return (
     <div
